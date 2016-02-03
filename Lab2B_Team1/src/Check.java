4d29854bbdf1ab948db0e6d332c58d49b604003e
Matthew <Matthew@75.102.112.77>
2016-02-03 09:56:31 -0500

    Added Richa's Check class.

---
+++ Lab2B_Team1/src/Check.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Richa
+ */
+public class Check{
+    private long receivingAccountNumber;
+    private long sendingAccountNumber;
+    private double checkAmount;
+    private int numberOfChecks;
+
+    Check(long receivingAccountNumber, long sendingAccountNumber, double checkAmount, int numberOfChecks)
+    {
+        this.receivingAccountNumber = receivingAccountNumber;
+        this.sendingAccountNumber = sendingAccountNumber;
+        this.checkAmount = checkAmount;
+        this.numberOfChecks = numberOfChecks;
+    }
+    
+    public long getReceivingAccountNumber() {
+        return receivingAccountNumber;
+    }
+
+
+    public long getSendingAccountNumber() {
+        return sendingAccountNumber;
+    }
+
+    public double getCheckAmount() {
+        return checkAmount;
+    }
+
+    public int getNumberOfChecks() {
+        return numberOfChecks;
+    }
+}
