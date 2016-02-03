fead946cbda01e1db2671a3445612b5a8107424c
lap5486 <lap5486@208-NWD12.up.ist.local>
2016-02-03 15:06:03 -0500

    Changed account to allow deposit/withdrawal

--- Lab2B_Team1/src/BusinessAccount.java
+++ Lab2B_Team1/src/BusinessAccount.java
@@ -10,10 +10,11 @@
  */
 public class BusinessAccount extends Account {
     private String BusinessName;
-    BusinessAccount(long AccountNumber,double AccountBalance, long CreditCard, int Pin, String BusinessName){
-        super(AccountNumber,AccountBalance,CreditCard,Pin);
+    BusinessAccount(long AccountNumber, double SavingsBalance, double CheckingBalance, long CreditCard,int Pin, String BusinessName){
+        super(AccountNumber,SavingsBalance, CheckingBalance,CreditCard,Pin);
         this.BusinessName = BusinessName;
     }
+    
     public String getAccountNumber(){
         return this.BusinessName + "'s Account Number is " + super.getAccountNumber();
     }
